4c5d9bad8d39fb449a096228073333d91f616688
devnook <devnook@gmail.com>
2013-03-26 19:46:39 +0000

    adding app

---
+++ temp/WebGL/shader-fs.js
@@ -0,0 +1,11 @@
+<!--
+//<script id="shader-fs" type="x-shader/x-fragment">
+    precision mediump float;
+
+    varying vec4 vColor;
+
+    void main(void) {
+        gl_FragColor = vColor;
+    }
+//</script>
+//-->
